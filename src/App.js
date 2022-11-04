1480c0b195e829e4997c7b21c23cd82fe22fe528
siwons01 <siwons01@naver.com>
2022-11-04 17:03:00 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setCount] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  console.log("run");
+  const onClick = () => setCount((prev) => prev+1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(
+    () => {console.log('useEffect')}
+    , []
+  );
+  useEffect(
+    
+      () => {
+        if("" !== keyword && keyword.length > 2) {
+          console.log('keyword :',  keyword)
+        }
+      }
+    , [keyword]
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={onChange} type="text" />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>onClick</button>
     </div>
   );
 }
